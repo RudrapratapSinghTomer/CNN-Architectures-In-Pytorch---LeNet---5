66fc17a328c0db04383f68f6e3e223eaead804af
RudrapratapSinghTomer <rudrawork01@gmail.com>
2026-01-29 09:11:30 +0530

    Commit Changes

--- main, NNP.py
+++ main, NNP.py
@@ -8,20 +8,14 @@ from torchvision import datasets, transforms
 class LeNet_(nn.Module):
     def __init__(self):
         super().__init__()
-        conv1 = nn.Conv2d(1, 6, 5)
-        conv2 = nn.Conv2d(6, 16, 5)
         fc1 = nn.Linear(16*4*4,120)
         fc2 = nn.Linear(120,84)
         fc3 = nn.Linear(84,10)
 
     def forward_pass(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.avg_pool2d(2, 2)
-
-        x = F.relu(self.conv2(x))
-        x = F.avg_pool2d(2, 2)
 
         x.flatten()
+        
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
